f3023eb2a72dfec6c746942231116da738b5092c
Woltvint <woltvint@gmail.com>
2019-05-26 00:57:42 +0200

    LeveL 04 + more small tweaks

---
+++ CheeseCube/Assets/Prefabs/Cannon/Cannon_Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon_Ball : MonoBehaviour
+{
+    public GameObject destroyParticles;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death" || collision.gameObject.tag == "Ground")
+        {
+            Instantiate(destroyParticles,transform.position,Quaternion.identity,transform.parent);
+            Destroy(gameObject);
+        }
+    }
+
+}
